fa06625f953a958d43bfa86f09039b7202190bb0
Harrycarter555 <top9pdf@gmail.com>
2024-07-07 21:19:10 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -7,36 +7,31 @@ api_key = "c3b95f90c469c2a1fbb4648e5fba9e800cf114f2"
 def search_movies(query):
     movies_list = []
     movies_details = {}
-    try:
-        website = BeautifulSoup(requests.get(f"https://mkvcinemas.cymru/?s={query.replace(' ', '+')}").text, "html.parser")
-        movies = website.find_all("a", {'class': 'ml-mask jt'})
-        for movie in movies:
+    website = BeautifulSoup(requests.get(f"https://mkvcinemas.cymru/?s={query.replace(' ', '+')}").text, "html.parser")
+    movies = website.find_all("a", {'class': 'ml-mask jt'})
+    for movie in movies:
+        if movie:
             movies_details["id"] = f"link{movies.index(movie)}"
             movies_details["title"] = movie.find("span", {'class': 'mli-info'}).text
             url_list[movies_details["id"]] = movie['href']
-            movies_list.append(movies_details)
-            movies_details = {}
-    except Exception as e:
-        print(f"Error searching movies: {e}")
+        movies_list.append(movies_details)
+        movies_details = {}
     return movies_list
 
 def get_movie(query):
     movie_details = {}
-    try:
-        movie_page_link = BeautifulSoup(requests.get(f"{url_list[query]}").text, "html.parser")
-        if movie_page_link:
-            title = movie_page_link.find("div", {'class': 'mvic-desc'}).h3.text
-            movie_details["title"] = title
-            img = movie_page_link.find("div", {'class': 'mvic-thumb'})['data-bg']
-            movie_details["img"] = img
-            links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
-            final_links = {}
-            for i in links:
-                url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
-                response = requests.get(url)
-                link = response.json()
-                final_links[f"{i.text}"] = link['shortenedUrl']
-            movie_details["links"] = final_links
-    except Exception as e:
-        print(f"Error getting movie details: {e}")
+    movie_page_link = BeautifulSoup(requests.get(f"{url_list[query]}").text, "html.parser")
+    if movie_page_link:
+        title = movie_page_link.find("div", {'class': 'mvic-desc'}).h3.text
+        movie_details["title"] = title
+        img = movie_page_link.find("div", {'class': 'mvic-thumb'})['data-bg']
+        movie_details["img"] = img
+        links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
+        final_links = {}
+        for i in links:
+            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
+            response = requests.get(url)
+            link = response.json()
+            final_links[f"{i.text}"] = link['shortenedUrl']
+        movie_details["links"] = final_links
     return movie_details
