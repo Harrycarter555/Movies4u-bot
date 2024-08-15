37b0b982046f9f24b6d93d642b4fa0765a0f9152
Harrycarter555 <top9pdf@gmail.com>
2024-08-15 11:54:34 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -6,25 +6,23 @@ api_key = "d15e1e3029f8e793ad6d02cf3343365ac15ad144"
 
 def search_movies(query):
     movies_list = []
-    movies_details = {}
     try:
         website = BeautifulSoup(requests.get(f"https://mkvcinemas.cat/?s={query.replace(' ', '+')}").text, "html.parser")
         movies = website.find_all("a", {'class': 'ml-mask jt'})
-        for movie in movies:
-            if movie:
-                movies_details["id"] = f"link{movies.index(movie)}"
-                movies_details["title"] = movie.find("span", {'class': 'mli-info'}).text
-                url_list[movies_details["id"]] = movie['href']
-                movies_list.append(movies_details)
-                movies_details = {}  # Reset after appending
+        for index, movie in enumerate(movies):
+            movie_details = {}
+            movie_details["id"] = f"link{index}"
+            movie_details["title"] = movie.find("span", {'class': 'mli-info'}).text
+            url_list[movie_details["id"]] = movie['href']
+            movies_list.append(movie_details)
     except Exception as e:
         print(f"[ERROR] Exception in search_movies: {e}")
     return movies_list
 
-def get_movie(query):
+def get_movie(movie_id):
     movie_details = {}
     try:
-        movie_page_link = BeautifulSoup(requests.get(f"{url_list[query]}").text, "html.parser")
+        movie_page_link = BeautifulSoup(requests.get(url_list[movie_id]).text, "html.parser")
         if movie_page_link:
             title = movie_page_link.find("div", {'class': 'mvic-desc'}).h3.text
             movie_details["title"] = title
@@ -37,11 +35,22 @@ def get_movie(query):
                 url = f"https://publicearn.com/api?api={api_key}&url={i['href']}"
                 response = requests.get(url)
                 link = response.json()
-                if 'watch online' in link_text:
-                    final_links[f"🔴 Watch Online"] = link['shortenedUrl']
-                else:
-                    final_links[f"{i.text}"] = link['shortenedUrl']
+                if 'shortenedUrl' in link:
+                    if 'watch online' in link_text:
+                        final_links[f"🔴 Stream Online Links:"] = link['shortenedUrl']
+                    else:
+                        final_links[f"{i.text}"] = link['shortenedUrl']
             movie_details["links"] = final_links
     except Exception as e:
         print(f"[ERROR] Exception in get_movie: {e}")
     return movie_details
+
+# Example usage
+query = "Stree 2"
+movies = search_movies(query)
+print("Movies List:", movies)
+
+if movies:
+    movie_id = movies[0]["id"]
+    movie = get_movie(movie_id)
+    print("Movie Details:", movie)
